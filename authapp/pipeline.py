c7d18d456980090b5fb8f25273be4f48a53117f5
Ilya Buryan <burian-iliyar@yandex.ru>
2021-07-08 12:31:41 +0300

    Initial

---
+++ authapp/pipeline.py
@@ -0,0 +1,53 @@
+import shutil
+from collections import OrderedDict
+from datetime import datetime
+from urllib.parse import urlencode, urlunparse
+
+import requests
+from django.utils import timezone
+from social_core.exceptions import AuthForbidden
+
+from authapp.models import ShopUserProfile
+
+
+def save_user_profile(backend, user, response, *args, **kwargs):
+    if backend.name != 'vk-oauth2':
+        return
+
+    api_url = urlunparse(('https',
+                          'api.vk.com',
+                          '/method/users.get',
+                          None,
+                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about', 'photo_200_orig')),
+                                                access_token=response['access_token'],
+                                                v='5.92')),
+                          None
+                          ))
+
+    resp = requests.get(api_url)
+    if resp.status_code != 200:
+        return
+
+    data = resp.json()['response'][0]
+    if data['sex']:
+        user.shopuserprofile.gender = ShopUserProfile.MALE if data['sex'] == 2 else ShopUserProfile.FEMALE
+
+    if data['about']:
+        user.shopuserprofile.aboutMe = data['about']
+
+    if data['bdate']:
+        bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
+
+        age = timezone.now().date().year - bdate.year
+        if age < 18:
+            user.delete()
+            raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+
+    if data['photo_200_orig']:
+        r = requests.get(data['photo_200_orig'], stream=True)
+        r.raw.decode_content = True
+        with open(f'media/users_avatars/{user.pk}.jpg', 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        user.avatar = str(user.pk) + ".jpg"
+
+    user.save()
---
+++ authapp/urls.py
@@ -0,0 +1,14 @@
+import authapp.views as authapp
+from django.urls import re_path, path
+
+app_name = "authapp"
+
+urlpatterns = [
+    re_path(r'^login/$', authapp.login, name='login'),
+    re_path(r'^logout/$', authapp.logout, name='logout'),
+    re_path(r'^register/$', authapp.register, name='register'),
+    re_path(r'^edit/$', authapp.edit, name='edit'),
+
+    path('verify/<str:email>/<str:activation_key>/', authapp.verify, name='verify'),
+
+]
