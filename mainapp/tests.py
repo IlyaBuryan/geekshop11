56f58d57e2b82bc8fea46760cccdf0998f367fc1
Ilya Buryan <burian-iliyar@yandex.ru>
2021-07-17 20:22:08 +0300

    Ex 5 mainapp test

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.test.client import Client
+from mainapp.models import Product, ProductCategory
 
-# Create your tests here.
+
+class TestMainappSmoke(TestCase):
+    def setUp(self):
+        cat = ProductCategory.objects.create(name='1st cat')
+        for i in range(10):
+            Product.objects.create(category=cat, name=f'{i + 1}st prod')
+        self.client = Client()
+
+    def test_mainapp_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/products/category/0/')
+        self.assertEqual(response.status_code, 200)
+
+        for category in ProductCategory.objects.all():
+            response = self.client.get(f'/products/category/{category.pk}/')
+            self.assertEqual(response.status_code, 200)
+
+        for product in Product.objects.all():
+            response = self.client.get(f'/products/product/{product.pk}/')
+            self.assertEqual(response.status_code, 200)
